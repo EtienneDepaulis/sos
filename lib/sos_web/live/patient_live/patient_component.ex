b0ca72b044f68b5262bcc9a968b3127678de4d52
Etienne Depaulis <etienne.depaulis@gmail.com>
2020-05-14 20:49:51 +0200

    Tailwind tryouts

--- lib/sos_web/live/patient_live/patient_component.ex
+++ lib/sos_web/live/patient_live/patient_component.ex
@@ -4,11 +4,11 @@ defmodule SosWeb.PatientLive.PatientComponent do
   def render(assigns) do
     ~L"""
       <tr class="<%= if new_item?(@new_patient_ids, @patient.id) do "new-item" end %>" id="patient-<%= @patient.id %>">
-        <td><%= @patient.first_name %></td>
-        <td><%= @patient.last_name %></td>
+        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-900"><%= @patient.first_name %></td>
+        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-900"><%= @patient.last_name %></td>
 
-        <td>
-          <span><%= live_redirect "Show", to: Routes.patient_show_path(@socket, :show, @patient) %></span>
+        <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+          <span><%= live_redirect "Show", to: Routes.patient_show_path(@socket, :show, @patient), class: "text-blue-600 hover:text-blue-900" %></span>
           <span><%= live_patch "Edit", to: Routes.patient_index_path(@socket, :edit, @patient) %></span>
           <span><%= link "Delete", to: "#", phx_click: "delete", phx_value_id: @patient.id, data: [confirm: "Are you sure?"] %></span>
         </td>
